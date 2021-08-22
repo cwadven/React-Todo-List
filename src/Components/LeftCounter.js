1157a30fbaffdc42588e2e8fcfa8664f84d19865
이창우 <cwadven@naver.com>
2021-08-22 14:42:06 +0900

    feat: LEFT TO DOS
    - 추가적으로 몇개가 남았는지 알려주는 Component 구현

---
+++ src/Components/LeftCounter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "@emotion/styled";
+import pepe from "../assets/pepe.gif"
+
+const Container = styled.div`
+    text-align: center;
+    margin-top: 30px;
+`;
+
+const Img = styled.img`
+    border-radius: 10px;
+`;
+
+const Bold = styled.b`
+    
+`;
+
+const LeftCounter = ({toDosCount, completedCount}) => {
+    return (
+        <Container>
+            <Bold>
+                LEFT TO DOS : {toDosCount}
+            </Bold>
+            <div>
+                🏃‍♂️
+                <progress id="file" max="100" value={Number(completedCount / (toDosCount + completedCount) * 100).toFixed(2)}/>
+                🏃‍♀️
+            </div>
+            <Img src={pepe}/>
+        </Container>
+    )
+}
+
+export default React.memo(LeftCounter);
