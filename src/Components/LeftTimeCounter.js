401eb25bc6b101891b4e6edfaf198588d6aa8c76
이창우 <cwadven@naver.com>
2021-08-23 00:50:43 +0900

    feat: used setInterval
    - setInterval to setState

--- src/Components/LeftTimeCounter.js
+++ src/Components/LeftTimeCounter.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styled from "@emotion/styled";
 
 const Container = styled.div`
+    position: relative;
     text-align: center;
     margin: 10px 0;
 `;
@@ -18,18 +19,7 @@ const Progress = styled.div`
     width: ${props => props.value}%;
     height: 10px;
     border-radius: 5px;
-    background-color: green;
-    animation-name: big${props => props.id};
-    animation-duration: ${props => props.leftTime}s;
-    
-    @keyframes big${props => props.id} {
-        from {
-          width: ${props => props.value}%;
-        }
-        to {
-          width: 100%;
-        }
-    }
+    background-color: ${props => props.value <= 45 ? "green" : props.value <= 50 ? "yellow" : props.value <= 70 ? "orange" : "red"};
 `;
 
 const Percentage = styled.b`
@@ -37,22 +27,58 @@ const Percentage = styled.b`
     text-align: center;
 `;
 
-const LeftTimeCounter = ({deadLine, startDate, id}) => {
+const Time = styled.div`
+    position: absolute;
+    font-size: 13px;
+    text-align: center;
+    right: 0;
+`;
+
+const LeftTimeCounter = ({deadLine, startDate}) => {
+    const [timer, setTimer] = useState();
+    const interval = useRef();
+
     const totalTime = (deadLine.getTime() / 1000) - (startDate.getTime() / 1000);
     const leftTime = (deadLine.getTime() / 1000) - (new Date().getTime() / 1000);
 
-    // 몇분 남음
-    // 몇초 남음 분기로 나누기
-    // 차츰 차츰 시간가는 거 보여주기 animate 로
-
     const percentage = Number(((totalTime - leftTime) / totalTime) * 100);
-    console.log(percentage, leftTime, id);
+
+    // 몇분 남음
+    const secondDisplay = (second) => {
+        let display;
+        if (second > 3600) {
+            display = `${Math.floor((second / 3600))}시간 ${((second % 3600) / 60).toFixed()}분 남음`;
+        } else if (second > 60) {
+            display = `${Math.floor((second / 60))}분 ${(second % 60).toFixed()}초 남음`;
+        } else if (second > 0) {
+            display = `${second.toFixed()}초 남음`;
+        } else {
+            display = `DeadLine`;
+        }
+        return display;
+    }
+
+    useEffect(() => {
+        interval.current = setInterval(() => setTimer(Date.now()), 1000);
+
+        return () => {
+            clearInterval(interval.current);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (leftTime <= 0) {
+            clearInterval(interval.current);
+        }
+    }, [timer])
+
     return (
         <Container>
             <ProgressBack>
-                <Progress value={percentage >= 100 ? "100" : percentage} leftTime={leftTime} id={id}/>
+                <Progress value={percentage >= 100 ? "100" : percentage} leftTime={leftTime}/>
             </ProgressBack>
-            <Percentage>{percentage >= 100 ? "100" : percentage.toFixed(0)} %</Percentage>
+            <Time>{secondDisplay(leftTime)}</Time>
+            <Percentage>{percentage >= 100 ? "100" : percentage.toFixed(1)} %</Percentage>
         </Container>
     )
 }
