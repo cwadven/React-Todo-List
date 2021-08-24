07edf8dfc2ea4042d8ef4294b3332dd94ef3b487
이창우 <cwadven@naver.com>
2021-08-24 22:49:54 +0900

    chore: context location changed
    - context location changed

--- src/index.js
+++ src/index.js
@@ -1,62 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ToDosProvider from "./context";
-import {Global, css} from '@emotion/react';
+import { Global, css } from '@emotion/react';
 
 const globalStyle = css`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-size: 20px;
-  }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 20px;
+    }
 
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
 
-  html,
-  body {
-    height: 100%;
-    background: #FAF3E0;
-  }
-  
-  input[name="toDoDeadLine"] {
-    font-size: 15px;
-    text-align: center;
-  }
-  
-  .react-datepicker__day-name, .react-datepicker__day {
-  margin: 0 !important;
-  font-size: 15px !important;
-}
+    html,
+    body {
+        height: 100%;
+        background: #faf3e0;
+    }
 
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
+    input[name='toDoDeadLine'] {
+        font-size: 15px;
+        text-align: center;
+    }
 
-  &::-webkit-scrollbar-thumb {
-    background-color: #B68973;
-    border-radius: 10px;
-    background-clip: padding-box;
-    border: 2px solid transparent;
-  }
+    .react-datepicker__day-name,
+    .react-datepicker__day {
+        margin: 0 !important;
+        font-size: 15px !important;
+    }
 
-  &::-webkit-scrollbar-track {
-    background-color: #dfdfdf;
-    border-radius: 10px;
-    box-shadow: inset 0px 0px 5px white;
-  }
+    &::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #b68973;
+        border-radius: 10px;
+        background-clip: padding-box;
+        border: 2px solid transparent;
+    }
+
+    &::-webkit-scrollbar-track {
+        background-color: #dfdfdf;
+        border-radius: 10px;
+        box-shadow: inset 0px 0px 5px white;
+    }
 `;
 
 ReactDOM.render(
     <React.StrictMode>
-        <Global styles={globalStyle}/>
-        <ToDosProvider>
-            <App/>
-        </ToDosProvider>
+        <Global styles={globalStyle} />
+        <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
