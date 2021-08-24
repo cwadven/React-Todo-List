d81fac50331915c9ba63d2e3bc9d424aa2d7f40d
이창우 <cwadven@naver.com>
2021-08-24 22:47:29 +0900

    chore: only login user can use
    - only login user can use

--- src/context.js
+++ src/context.js
@@ -1,50 +1,72 @@
-import React, {createContext, useContext, useEffect, useReducer} from "react";
-import reducer, {SET_COMPLETED, SET_TODO, initialState} from "./reducer";
-import ToDoModel from "./models/ToDoModel";
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
+import reducer, { SET_COMPLETED, SET_TODO, initialState } from './reducer';
+import ToDoModel from './models/ToDoModel';
+import { withRouter } from 'react-router-dom';
+import { errorResponse } from './models/AccountModel';
 
 const ToDosContext = createContext();
 
-const ToDosProvider = ({children}) => {
+const ToDosProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const getToDoList = async () => {
-        const {data: {todo_set}} = await ToDoModel.getToDoList();
-        console.log(todo_set)
-        return todo_set;
-    }
+        try {
+            const {
+                data: { todo_set },
+            } = await ToDoModel.getToDoList();
+            return todo_set;
+        } catch (e) {
+            console.log(e);
+            errorResponse(e.response.data);
+        }
+    };
 
     const getCompletedList = async () => {
-        const {data: {completed_set}} = await ToDoModel.getCompletedList();
-        return completed_set;
-    }
+        try {
+            const {
+                data: { completed_set },
+            } = await ToDoModel.getCompletedList();
+            return completed_set;
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     // eslint-disable-next-line
-    useEffect(async ()=>{
+    useEffect(async () => {
         const todoSet = await getToDoList();
         const completedSet = await getCompletedList();
-        dispatch({type: SET_TODO, payload: todoSet});
-        dispatch({type: SET_COMPLETED, payload: completedSet});
+        dispatch({ type: SET_TODO, payload: todoSet });
+        dispatch({ type: SET_COMPLETED, payload: completedSet });
     }, []);
 
-    return <ToDosContext.Provider value={{state, dispatch}}>{children}</ToDosContext.Provider>;
-}
+    return (
+        <ToDosContext.Provider value={{ state, dispatch }}>
+            {children}
+        </ToDosContext.Provider>
+    );
+};
 
 // reducer 로 받은 state 바꾸는 것 return 해서 사용하기
 export const useDispatch = () => {
-    const {dispatch} = useContext(ToDosContext);
+    const { dispatch } = useContext(ToDosContext);
     return dispatch;
-}
+};
 
 // reducer 로 받은 state 중 toDos 만 사용하기
 export const useToDos = () => {
-    const {state: {toDos}} = useContext(ToDosContext);
+    const {
+        state: { toDos },
+    } = useContext(ToDosContext);
     return toDos;
-}
+};
 
 // reducer 로 받은 state 중 completed 만 사용하기
 export const useCompleted = () => {
-    const {state: {completed}} = useContext(ToDosContext);
+    const {
+        state: { completed },
+    } = useContext(ToDosContext);
     return completed;
-}
+};
 
-export default ToDosProvider;
+export default withRouter(ToDosProvider);
