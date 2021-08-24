b398b9f49d126496d61c0fc455d8e7c2f2c4a689
이창우 <cwadven@naver.com>
2021-08-24 22:45:35 +0900

    chore: setting login api
    - setting login api

--- src/models/AccountModel.js
+++ src/models/AccountModel.js
@@ -5,21 +5,18 @@ const api = axios.create({
 });
 
 export const getToken = () => window.sessionStorage.getItem('__AUTH__');
-export const setToken = (token) => window.sessionStorage.setItem('__AUTH__', token);
-
-export const getAccesesToken = () => {
-    if (!getToken()) {
-        const {data: {token}} = AccountModel.login();
-        setToken(token);
+export const setToken = token =>
+    window.sessionStorage.setItem('__AUTH__', `jwt ${token}`);
+export const errorResponse = errResponseData => {
+    // 로그인 애러
+    if (Object.values(errResponseData).some(msg => msg === 'No Auth')) {
+        alert('No Auth Login Please');
+        window.location.replace('/');
     }
-    return `jwt ${getToken()}`;
 };
 
 const AccountModel = {
-    login: (data) => api.post('accounts/login', {
-        username: 'root',
-        password: 'root',
-    }),
-}
+    login: data => api.post('accounts/login', data),
+};
 
 export default AccountModel;
