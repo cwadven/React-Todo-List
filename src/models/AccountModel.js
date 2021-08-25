80ddc70ce53d8f5f84991d8187677c50338e204a
이창우 <cwadven@naver.com>
2021-08-26 00:12:16 +0900

    feat: 로그인 및 토큰 만료 시, session 제거
    - 무한 에러 방지

--- src/models/AccountModel.js
+++ src/models/AccountModel.js
@@ -8,9 +8,20 @@ export const getToken = () => window.sessionStorage.getItem('__AUTH__');
 export const setToken = token =>
     window.sessionStorage.setItem('__AUTH__', `jwt ${token}`);
 export const errorResponse = errResponseData => {
-    // 로그인 애러
+    // 로그인 에러
     if (Object.values(errResponseData).some(msg => msg === 'No Auth')) {
         alert('No Auth Login Please');
+        window.sessionStorage.clear();
+        window.location.replace('/');
+    }
+    // 토큰 에러
+    if (
+        Object.values(errResponseData).some(
+            msg => msg === 'Error decoding signature.',
+        )
+    ) {
+        alert('No Auth Login Please');
+        window.sessionStorage.clear();
         window.location.replace('/');
     }
 };
