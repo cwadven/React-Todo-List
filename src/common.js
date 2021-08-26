04d2e02070b268ae637f710bc724cb909002cddd
이창우 <cwadven@naver.com>
2021-08-26 23:08:57 +0900

    feat: common func file created
    - 공통으로 쓰는 함수 모음집 파일 생성

---
+++ src/common.js
@@ -0,0 +1,39 @@
+export const arrayInObjectSort = (array, key, method = 'asc', type = 'int') => {
+    return array.sort((a, b) => {
+        if (type === 'date') {
+            if (method === 'asc') {
+                if (new Date(a[key]) > new Date(b[key])) {
+                    return 1;
+                }
+                if (new Date(a[key]) < new Date(b[key])) {
+                    return -1;
+                }
+            } else {
+                if (new Date(a[key]) > new Date(b[key])) {
+                    return -1;
+                }
+                if (new Date(a[key]) < new Date(b[key])) {
+                    return 1;
+                }
+            }
+            return 0;
+        } else {
+            if (method === 'asc') {
+                if (a[key] > b[key]) {
+                    return 1;
+                }
+                if (a[key] < b[key]) {
+                    return -1;
+                }
+            } else {
+                if (a[key] > b[key]) {
+                    return -1;
+                }
+                if (a[key] < b[key]) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    });
+};
