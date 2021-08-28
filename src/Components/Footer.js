aec17070c2a302a5026e545cb1624046fb51f9c8
이창우 <cwadven@naver.com>
2021-08-29 00:06:39 +0900

    chore: Footer style
    - font size changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,25 +2,29 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const FooterWrapper = styled.footer`
-  display: grid;
-  background: #f5f5f5;
-  place-items: center;
-  margin-top: auto;
-  padding: 50px 0;
-  font-size: 15px;
-  text-align: center;
-  line-height: 1.5;
+    display: grid;
+    background: #f5f5f5;
+    place-items: center;
+    margin-top: auto;
+    padding: 50px 0;
+    font-size: 15px;
+    text-align: center;
+    line-height: 1.5;
+`;
+
+const Span = styled.span`
+    font-size: 15px;
 `;
 
 const Footer = () => {
-  return (
-    <FooterWrapper>
-      <span role="img" aria-label="like">
-        Thank You for Visiting Site, Have a Good Day 😆 <br /> © 2021
-        Developer Nully Powered By React.
-      </span>
-    </FooterWrapper>
-  );
+    return (
+        <FooterWrapper>
+            <Span role="img" aria-label="like">
+                Thank You for Visiting Site, Have a Good Day 😆 <br /> © 2021
+                Developer Nully Powered By React.
+            </Span>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
