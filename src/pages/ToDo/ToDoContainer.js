4dd90e90f2115e986dc8bf8fed4fcbe8657dac91
cwadven <cwadven@naver.com>
2022-05-15 12:21:15 +0900

    chore: 만약 로그인 하지 않은 경우
    - 로그인 안한 사람이 Todo에 접근하는 경우 다시 로그인 화면으로

--- src/pages/ToDo/ToDoContainer.js
+++ src/pages/ToDo/ToDoContainer.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ToDoPresenter from './ToDoPresenter';
 import { getIsPending, useCategory, useCompleted, useToDos } from '../../context';
+import { getToken } from '../../models/AccountModel';
 
 const ToDoContainer = () => {
     const toDos = useToDos();
     const completed = useCompleted();
     const categorySet = useCategory();
     const isPending = getIsPending();
+
+    useEffect(() => {
+        const loginToken = getToken();
+        if (!loginToken) {
+            window.location.replace('/');
+        }
+    }, [])
 
     return (
         <ToDoPresenter
