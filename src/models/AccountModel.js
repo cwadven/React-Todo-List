b1f4883586ace2d502451dcdfec109294fb8fe51
cwadven <cwadven@naver.com>
2022-05-15 12:23:53 +0900

    fix: baseURL 수정
    - baseURL 수정

--- src/models/AccountModel.js
+++ src/models/AccountModel.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://record-to-do-list.shop/',
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'https://record-to-do-list.shop/',
+    // baseURL: 'http://127.0.0.1:8000/',
 });
 
 export const getToken = () => window.sessionStorage.getItem('__AUTH__');
